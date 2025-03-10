c0267d1b78b89adcd366da23166579e9431aa7c6
Priyanshu151206 <priyanshu.ror1809@gmail.com>
2025-03-10 17:25:57 +0530

    Solution to Input and Print a String

--- Input and Print a String@@6788d5012db25efec946f93c/code.c
+++ Input and Print a String@@6788d5012db25efec946f93c/code.c
@@ -4,12 +4,6 @@
 int main() {
     int N[];
     scanf("%s", &N);
-
-    if (N <= 0) {
-        printf(stderr, "Error: Value must be positive.\n");
-        exit(EXIT_FAILURE);  // Exit the program with failure status
-    }
-
     printf("Valid input: %s\n", N);
     return 0;
 }
