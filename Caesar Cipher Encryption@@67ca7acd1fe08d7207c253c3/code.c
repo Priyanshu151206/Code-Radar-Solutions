30b3cdc2c65552dd36eb41bf1c4cefc979fac900
Priyanshu151206 <priyanshu.ror1809@gmail.com>
2025-03-21 00:08:26 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -3,6 +3,5 @@
 #include<string.h>
 char *caesarCipher(char str[],int n,char encrypted[]){
     for(int i=0;i<strlen(str);i++){
-        encrypted[i]=str[i]+n;
-        }
-   return encrypted;}
+        encrypted[i]=str[i]+n;}
+    return encrypted;}
